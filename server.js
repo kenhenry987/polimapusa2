031afd6dba4b260a8b419dc1e6755bed1358d840
kenhenry987 <kenhenry123@gmail.com>
2022-10-19 16:21:54 -0400

    db access account

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const os = require('os');
-//const mysql = require('mysql');
+const mysql = require('mysql');
 const PORT = process.env.PORT || 3000;
 
-/*const db = mysql.createConnection({
+const db = mysql.createConnection({
 host : 'localhost', 
-user : 'rroot',
+user : 'pmreader',
 password : 'password1',
 database : 'polimap'
-});*/
+});
     
 /*db.connect((err) => {
 if(err){ 
@@ -20,19 +20,19 @@ console.log('mysql connected...');
 const app = express();
 
  app.get('/apig/:ids', (req,res) => {
-  //  //req.params.idparturl , req.query.varquerystring
-  //  let sname = req.query.sname; 
-  //  let sql= "select * from persons where family_name > 'aa' order by family_name Limit 10 ";
-  //  if( sname != '')   sql = sql.replace( 'aa', sname);
-  //  let query = db.query(sql, (err,results) => {
-  //      if(err) throw err;
-  //      let resultx = Object.values(JSON.parse(JSON.stringify(results)));
-  //      let sbuf ="";
-  //      resultx.forEach((v) => sbuf+=JSON.stringify(v) + '<br>');
-  //     res.header('Access-Control-Allow-Origin',"*");
-  //     res.send('post fetched..' + sbuf);
-  //    // res.send("post fetched simple..");
-  //  });
+    //req.params.idparturl , req.query.varquerystring
+    let sname = req.query.sname; 
+    let sql= "select * from persons where family_name > 'aa' order by family_name Limit 10 ";
+    if( sname != '')   sql = sql.replace( 'aa', sname);
+    let query = db.query(sql, (err,results) => {
+        if(err) throw err;
+        let resultx = Object.values(JSON.parse(JSON.stringify(results)));
+        let sbuf ="";
+        resultx.forEach((v) => sbuf+=JSON.stringify(v) + '<br>');
+       res.header('Access-Control-Allow-Origin',"*");
+       res.send('post fetched..' + sbuf);
+    // res.send("post fetched simple..");
+    });
 });
 
 app.get('/apidate/:idd' , (req,res) => {
@@ -41,6 +41,12 @@ app.get('/apidate/:idd' , (req,res) => {
       //let s = "time = " + Date() ;
       res.send(s);
 });
+
+
+//var app = express();
+//app.get('/test', function(req, res) {
+//    res.sendFile('views/test.html', {root: __dirname })
+//});
 
 app.listen( PORT, () => {
  console.log('Server started on port 3000');
